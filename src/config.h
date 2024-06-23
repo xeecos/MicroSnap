3cecf57579370327d90a28aff8813a9377fa93aa
xeecos <flashindream@gmail.com>
2024-06-23 15:14:29 +0800

    test stepper

---
+++ src/config.h
@@ -0,0 +1,11 @@
+#pragma once
+//PORT B
+#define PIN_STEPPER_EN      GPIO_Pin_7
+#define PIN_LED             GPIO_Pin_14
+//PORT A
+#define PIN_STEPPER_DIR     GPIO_Pin_14
+#define PIN_STEPPER_STEP    GPIO_Pin_15
+#define PIN_SHOT            GPIO_Pin_12
+#define PIN_FOCUS           GPIO_Pin_13
+#define PIN_TX3             GPIO_Pin_5
+#define PIN_RX3             GPIO_Pin_4
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,5 @@
-/********************************** (C) COPYRIGHT *******************************
- * File Name          : main.c
- * Author             : WCH
- * Version            : V1.1
- * Date               : 2022/05/10
- * Description        : FreeRTOS例程，使用硬件压栈，中断嵌套可选，中断函数不再使用修饰
- *                      __attribute__((interrupt("WCH-Interrupt-fast")))，
- *                      中断函数直接按照普通函数定义，只使用HIGHCODE修饰即可。
- * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * SPDX-License-Identifier: Apache-2.0
- *******************************************************************************/
-
-/******************************************************************************/
-/* 头文件包含 */
-#if defined(CH58X)
 #include "CH58x_common.h"
-#elif defined(CH59X)
-#include "CH59x_common.h"
-#endif
-
-#define BLINKY_GPIO_PIN  GPIO_Pin_14
-
-
-/*********************************************************************
- * @fn      main
- *
- * @brief   main function.
- *
- * @param   none
- *
- * @return  none
- */
+#include "config.h"
 int main(void)
 {
 #if(defined(DCDC_ENABLE)) && (DCDC_ENABLE == TRUE)
@@ -37,13 +7,32 @@ int main(void)
 #endif
     SetSysClock(CLK_SOURCE_PLL_60MHz);
     /* for blinky */
-    GPIOB_ModeCfg(BLINKY_GPIO_PIN, GPIO_ModeOut_PP_20mA);
-    GPIOB_SetBits(BLINKY_GPIO_PIN);
+    GPIOA_ModeCfg(PIN_STEPPER_DIR|PIN_STEPPER_STEP, GPIO_ModeOut_PP_20mA);
+    GPIOB_ModeCfg(PIN_LED|PIN_STEPPER_EN, GPIO_ModeOut_PP_20mA);
+    GPIOB_SetBits(PIN_LED);
+    GPIOB_ResetBits(PIN_STEPPER_EN);
+    GPIOA_SetBits(PIN_STEPPER_DIR);
+    for(int i = 0; i < 1000; i++)
+    {
+        GPIOA_SetBits(PIN_STEPPER_STEP);
+        DelayUs(1);
+        GPIOA_ResetBits(PIN_STEPPER_STEP);
+        DelayUs(400);
+    }
+    GPIOA_ResetBits(PIN_STEPPER_DIR);
+    for(int i = 0; i < 1000; i++)
+    {
+        GPIOA_SetBits(PIN_STEPPER_STEP);
+        DelayUs(1);
+        GPIOA_ResetBits(PIN_STEPPER_STEP);
+        DelayUs(400);
+    }
+    GPIOB_SetBits(PIN_STEPPER_EN);
     while (1)
     {
-        GPIOB_SetBits(BLINKY_GPIO_PIN);
+        GPIOB_SetBits(PIN_LED);
         DelayMs(1000);
-        GPIOB_ResetBits(BLINKY_GPIO_PIN);
+        GPIOB_ResetBits(PIN_LED);
         DelayMs(1000);
     }
 }
