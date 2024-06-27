fc52e7c1ce071810a6d2c57714823c9570b99825
虎子哥 <flashindream@gmail.com>
2024-06-27 18:13:04 +0800

    update comment

--- lib/BLE_HAL/include/config.h
+++ lib/BLE_HAL/include/config.h
@@ -3,9 +3,11 @@
  * Author             : WCH
  * Version            : V1.2
  * Date               : 2022/01/18
- * Description        : ����˵����Ĭ��ֵ�������ڹ����������Ԥ�������޸ĵ�ǰֵ
+ * Description        : 配置说明及默认值，建议在工程配置里的预处理中修改当前值
+ *********************************************************************************
  * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * SPDX-License-Identifier: Apache-2.0
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
  *******************************************************************************/
 
 /******************************************************************************/
@@ -25,51 +27,53 @@
 #include "CH58x_common.h"
 
 /*********************************************************************
- ��MAC��
- BLE_MAC                                    - �Ƿ��Զ�������Mac��ַ ( Ĭ��:FALSE - ʹ��оƬMac��ַ )����Ҫ��main.c�޸�Mac��ַ����
+ 【MAC】
+ BLE_MAC                                    - 是否自定义蓝牙Mac地址 ( 默认:FALSE - 使用芯片Mac地址 )，需要在main.c修改Mac地址定义
 
- ��DCDC��
- DCDC_ENABLE                                - �Ƿ�ʹ��DCDC ( Ĭ��:FALSE )
+ 【DCDC】
+ DCDC_ENABLE                                - 是否使能DCDC ( 默认:FALSE )
 
- ��SLEEP��
- HAL_SLEEP                                  - �Ƿ���˯�߹��� ( Ĭ��:FALSE )
- SLEEP_RTC_MIN_TIME                         - �ǿ���ģʽ��˯�ߵ���Сʱ�䣨��λ��625us��
- WAKE_UP_RTC_MAX_TIME                       - �ȴ�32M�����ȶ�ʱ�䣨��λ��625us��
-                                                ���ݲ�ͬ˯������ȡֵ�ɷ�Ϊ�� ˯��ģʽ/�µ�ģʽ  - 45(Ĭ��)
-                                                                          ��ͣģʽ    - 45
-                                                                          ����ģʽ    - 5
- ��TEMPERATION��
- TEM_SAMPLE                                 - �Ƿ�򿪸����¶ȱ仯У׼�Ĺ��ܣ�����У׼��ʱС��10ms( Ĭ��:TRUE )
+ 【SLEEP】
+ HAL_SLEEP                                  - 是否开启睡眠功能 ( 默认:FALSE )
+ SLEEP_RTC_MIN_TIME                         - 非空闲模式下睡眠的最小时间（单位：一个RTC周期）
+ SLEEP_RTC_MAX_TIME                         - 非空闲模式下睡眠的最大时间（单位：一个RTC周期）
+ WAKE_UP_RTC_MAX_TIME                       - 等待32M晶振稳定时间（单位：一个RTC周期）
+                                                                                                                            根据不同睡眠类型取值可分为： 睡眠模式/下电模式  - 45 (默认)
+                                                                                                                                                                                                  暂停模式    - 45
+                                                                                                                                                                                                  空闲模式    - 5
+ 【TEMPERATION】
+ TEM_SAMPLE                                 - 是否打开根据温度变化校准的功能，单次校准耗时小于10ms( 默认:TRUE )
  
- ��CALIBRATION��
- BLE_CALIBRATION_ENABLE                     - �Ƿ�򿪶�ʱУ׼�Ĺ��ܣ�����У׼��ʱС��10ms( Ĭ��:TRUE )
- BLE_CALIBRATION_PERIOD                     - ��ʱУ׼�����ڣ���λms( Ĭ��:120000 )
+ 【CALIBRATION】
+ BLE_CALIBRATION_ENABLE                     - 是否打开定时校准的功能，单次校准耗时小于10ms( 默认:TRUE )
+ BLE_CALIBRATION_PERIOD                     - 定时校准的周期，单位ms( 默认:120000 )
  
- ��SNV��
- BLE_SNV                                    - �Ƿ���SNV���ܣ����ڴ������Ϣ( Ĭ��:TRUE )
- BLE_SNV_ADDR                               - SNV��Ϣ�����ַ��ʹ��data flash���( Ĭ��:0x77E00 )
-                                            - ���������SNVNum����������Ҫ��Ӧ�޸�Lib_Write_Flash�����ڲ�����flash��С����СΪSNVBlock*SNVNum
+ 【SNV】
+ BLE_SNV                                    - 是否开启SNV功能，用于储存绑定信息( 默认:TRUE )
+ BLE_SNV_ADDR                               - SNV信息保存地址，使用data flash最后512字节( 默认:0x77E00 )
+ BLE_SNV_BLOCK                              - SNV信息保存块大小( 默认:256 )
+ BLE_SNV_NUM                                - SNV信息保存数量( 默认:1 )
 
- ��RTC��
- CLK_OSC32K                                 - RTCʱ��ѡ�������������ɫ����ʹ���ⲿ32K( 0 �ⲿ(32768Hz)��Ĭ��:1���ڲ�(32000Hz)��2���ڲ�(32768Hz) )
+ 【RTC】
+ CLK_OSC32K                                 - RTC时钟选择，如包含主机角色必须使用外部32K( 0 外部(32768Hz)，默认:1：内部(32000Hz)，2：内部(32768Hz) )
 
- ��MEMORY��
- BLE_MEMHEAP_SIZE                           - ����Э��ջʹ�õ�RAM��С����С��6K ( Ĭ��:(1024*6) )
+ 【MEMORY】
+ BLE_MEMHEAP_SIZE                           - 蓝牙协议栈使用的RAM大小，不小于6K ( 默认:(1024*6) )
 
- ��DATA��
- BLE_BUFF_MAX_LEN                           - ����������������( Ĭ��:27 (ATT_MTU=23)��ȡֵ��Χ[27~516] )
- BLE_BUFF_NUM                               - ����������İ�����( Ĭ��:5 )
- BLE_TX_NUM_EVENT                           - ���������¼������Է����ٸ����ݰ�( Ĭ��:1 )
- BLE_TX_POWER                               - ���书��( Ĭ��:LL_TX_POWEER_0_DBM (0dBm) )
+ 【DATA】
+ BLE_BUFF_MAX_LEN                           - 单个连接最大包长度( 默认:27 (ATT_MTU=23)，取值范围[27~516] )
+ BLE_BUFF_NUM                               - 控制器缓存的包数量( 默认:5 )
+ BLE_TX_NUM_EVENT                           - 单个连接事件最多可以发多少个数据包( 默认:1 )
+ BLE_TX_POWER                               - 发射功率( 默认:LL_TX_POWEER_0_DBM (0dBm) )
  
- ��MULTICONN��
- PERIPHERAL_MAX_CONNECTION                  - ����ͬʱ�����ٴӻ���ɫ( Ĭ��:1 )
- CENTRAL_MAX_CONNECTION                     - ����ͬʱ������������ɫ( Ĭ��:3 )
+ 【MULTICONN】
+ PERIPHERAL_MAX_CONNECTION                  - 最多可同时做多少从机角色( 默认:1 )
+ CENTRAL_MAX_CONNECTION                     - 最多可同时做多少主机角色( 默认:3 )
 
  **********************************************************************/
 
 /*********************************************************************
- * Ĭ������ֵ
+ * 默认配置值
  */
 #ifndef BLE_MAC
 #define BLE_MAC                             FALSE
@@ -108,7 +112,7 @@
 #define BLE_SNV_ADDR                        0x77E00-FLASH_ROM_MAX_SIZE
 #endif
 #ifndef CLK_OSC32K
-#define CLK_OSC32K                          1   // ���������ڴ��޸ģ������ڹ����������Ԥ�������޸ģ������������ɫ����ʹ���ⲿ32K
+#define CLK_OSC32K                          1   // 该项请勿在此修改，必须在工程配置里的预处理中修改，如包含主机角色必须使用外部32K
 #endif
 #ifndef BLE_MEMHEAP_SIZE
 #define BLE_MEMHEAP_SIZE                    (1024*6)
